9b9eb4f2f6131d3a402a2e84139c0bb569ebe560
Ameen Ul Islam <37907736+ameenulislam@users.noreply.github.com>
2020-04-01 21:58:02 +0530

    Created bottom and sliding navigation bar

---
+++ app/src/main/java/com/cuboid/rentacabs/booking.java
@@ -0,0 +1,14 @@
+package com.cuboid.rentacabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class booking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+    }
+}
